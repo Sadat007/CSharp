ccb0ca5890d2f3924816df69a0daabebb71c43f8
mukulmis <mukul_iiuc@yahoo.com>
2017-09-11 12:53:56 +0200

    While loop Test

---
+++ MultipliyerLib/Multipliyer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultipliyerLib
+{
+    public class Multipliyer
+    {
+        public long Simple(long left, long right)
+        {
+            return left * right;
+        }
+        public long Usingfor(long left, long right)
+        {
+
+            return left * right;
+        }
+        public long While(long left, long right)
+        {
+            long total = 0;
+            long loopVar = 0;
+            while (loopVar < left)
+            {
+                total += right;
+                loopVar++;
+            }
+
+            return total;
+        }
+    }
+}
---
+++ MultipliyerTests/MultipliyerTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MultipliyerLib;
+
+namespace MultipliyerTestsLib
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Simple()
+        {
+            var sut = new Multipliyer();
+            var actual = sut.Simple(20, 20);
+            Assert.AreEqual(400, actual);
+        }
+        [TestMethod]
+        public void Usingfor()
+        {
+            var sut = new Multipliyer();
+            var actual = sut.Usingfor(20, 20);
+            Assert.AreEqual(400, actual);
+        }
+        [TestMethod]
+        public void While()
+        {
+            var sut = new Multipliyer();
+            var actual = sut.While(20, 20);
+            Assert.AreEqual(400, actual);
+        }
+    }
+}
